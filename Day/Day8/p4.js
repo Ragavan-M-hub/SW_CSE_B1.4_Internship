e782bcb4b414b8f0c47faaefd7c01f6884f9c8c7
Ragavan-M-hub <ragavan.am2007@gmail.com>
2026-07-22 12:55:38 +0530

    const in JS

--- Day/Day8/p4.js
+++ Day/Day8/p4.js
@@ -11,7 +11,7 @@ console.log(message)
 
 //let
 let score = 100
-// let score = 100 ERROR
+// let score = 200 ERROR
 score = 160
 
 console.log(score)
@@ -21,3 +21,11 @@ if(true){
 }
 //let is block scoped
 console.log(secret)
+
+//const
+if(true){
+    const pi=3.1415
+    // pi=22 ERROR
+}
+//const is block scoped
+console.log(pi) //ERROR
