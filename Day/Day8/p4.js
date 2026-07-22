964af4227a815db719883291d80b0d08db725f0d
Ragavan-M-hub <ragavan.am2007@gmail.com>
2026-07-22 12:49:02 +0530

    Let in JS

--- Day/Day8/p4.js
+++ Day/Day8/p4.js
@@ -6,5 +6,18 @@ console.log(x)
 if(true){
     var message = "Hello"
 }
-
+//var is not block scoped
 console.log(message)
+
+//let
+let score = 100
+// let score = 100 ERROR
+score = 160
+
+console.log(score)
+
+if(true){
+    let secret = "hello"
+}
+//let is block scoped
+console.log(secret)
