6a609fa1685f3ca949d2132c524530e5054d4d59
Ragavan-M-hub <ragavan.am2007@gmail.com>
2026-07-30 12:53:37 +0530

    Hook ex1

--- Day/Day14/day14-react-app/src/App.jsx
+++ Day/Day14/day14-react-app/src/App.jsx
@@ -2,14 +2,16 @@ import './App.css'
 import SimpleList from './components/List1'
 import UserList from './components/List2'
 import ProductListDemo from './components/List3'
+import CounterHook from './components/Hook1'
 
 function App() {
 
   return (
     <>
-      {/* <SimpleList /> */}
-      {/* <UserList /> */}
+      <SimpleList />
+      <UserList />
       <ProductListDemo />
+      <CounterHook />
     </>
   )
 }
