90f8dd53b7aeef4d528b24e2e9a269188055ed0b
Ragavan-M-hub <ragavan.am2007@gmail.com>
2026-07-30 15:27:24 +0530

    Hook ex3

--- Day/Day14/day14-react-app/src/App.jsx
+++ Day/Day14/day14-react-app/src/App.jsx
@@ -4,16 +4,18 @@ import UserList from './components/List2'
 import ProductListDemo from './components/List3'
 import CounterHook from './components/Hook1'
 import UserListHook from './components/Hook2'
+import ThemeProviderDemo from './components/Hook3'
 
 function App() {
 
   return (
     <>
-      <SimpleList />
+      {/* <SimpleList />
       <UserList />
       <ProductListDemo />
       <CounterHook />
-      <UserListHook />
+      <UserListHook /> */}
+      <ThemeProviderDemo />
     </>
   )
 }
