a4435986526dcfcbca36e71eaccd76b3d60d795c
Ragavan-M-hub <ragavan.am2007@gmail.com>
2026-08-04 08:01:44 +0530

    Notfound Updated

--- Day/Day15/react-portfolio/src/pages/NotFound.jsx
+++ Day/Day15/react-portfolio/src/pages/NotFound.jsx
@@ -1,9 +1,13 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function NotFound() {
 return (
 <div style={{ textAlign: 'center', padding: '100px' }}>
-{'404 Page Not FoundBack to Home'}
+{'404 Page Not Found'}
+<br />
+<NavLink to="/" onClick={() => setIsMenuOpen(false)} className={({ isActive }) => isActive ? 'active' : ''}>
+    Back to Home
+</NavLink>
 </div>
 );
 }
